dd8ff286f1302cafdf8b973087b19415794e1b72
WilliamBendtsen <eaa23wfb@students.eaaa.dk>
2026-04-23 08:39:40 +0200

    remove comments

--- lib/twitchProxy.ts
+++ lib/twitchProxy.ts
@@ -1,11 +1,9 @@
 import { supabase } from "./supabase";
 
-/* This looks in my .env file for a point towards the name of the edge function */
 const twitchProxyFunction =
   process.env.EXPO_PUBLIC_TWITCH_PROXY_FUNCTION ?? "hyper-handler";
 
-/* This is type definition for the data returned from the api (in object form) */
-type TwitchGame = {
+export type TwitchGame = {
   id?: number | string;
   name?: string;
   coverUrl?: string;
@@ -16,21 +14,17 @@ type TwitchGame = {
   platformName?: string;
 };
 
-/* This is type definition for the return of the data from `TwitchGame` */
 type TwitchProxyResponse = {
   results?: TwitchGame[];
 };
 
-/* Here we make a function to search games from the api, with a proimse that the `TwitchGame` will eventually return an array of "TwitchGame" objects */
 export async function searchTwitchGames(query: string): Promise<TwitchGame[]> {
-  /* We start by trimming the response */
   const trimmed = query.trim();
 
   if (!trimmed) {
     return [];
   }
 
-  /* Then we use some built in supabase function to invoke the api call? */
   let { data, error } = await supabase.functions.invoke<TwitchProxyResponse>(
     twitchProxyFunction,
     {
@@ -66,12 +60,10 @@ export async function searchTwitchGames(query: string): Promise<TwitchGame[]> {
     throw new Error(fallbackMessage);
   }
 
-  /* Then if the data pulled is from the "results" part of the edge function, we just return said raw results, since they're already parsed */
   if (Array.isArray(data?.results)) {
     return data.results;
   }
 
-  /* And if the data is jst from the "data" part of the edge function, we return the data with the parameters defined in the "TwitchGame" type definition */
   if (Array.isArray(data)) {
     return data as TwitchGame[];
   }
